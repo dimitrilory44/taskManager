10d9ab6a5a419b6e2c297d1e3d0eb7494e9b128e
LORY <lorydim44@gmail.com>
2025-04-22 12:22:39 +0200

    chore: problème indentation

--- backend/src/main/java/com/dim/task/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/dim/task/exception/GlobalExceptionHandler.java
@@ -24,82 +24,82 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
  */
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-	
-	/**
+
+    /**
      * Gère l'exception {@link EmailAlreadyUsedException} lorsque l'email est déjà enregistré.
      *
      * @param ex l'exception levée contenant le message d'erreur
      * @return une réponse HTTP 400 (Bad Request) contenant les détails de l'erreur
      */
-	@ExceptionHandler(EmailAlreadyUsedException.class)
+    @ExceptionHandler(EmailAlreadyUsedException.class)
     public ResponseEntity<Object> handleEmailAlreadyUsed(EmailAlreadyUsedException ex) {
-		return buildResponse(HttpStatus.CONFLICT, "Le compte existe déjà avec l'email fourni", ex.getMessage(), null);
+        return buildResponse(HttpStatus.CONFLICT, "Le compte existe déjà avec l'email fourni", ex.getMessage(), null);
     }
-	
-	@ExceptionHandler(UserNotFoundException.class)
+
+    @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<Object> handleUserNotFound(UserNotFoundException ex) {
-		return buildResponse(HttpStatus.NOT_FOUND, "Aucun compte ne correspond à l'email fourni.", ex.getMessage(), null);
+        return buildResponse(HttpStatus.NOT_FOUND, "Aucun compte ne correspond à l'email fourni.", ex.getMessage(), null);
     }
-	
-	// 400
-	@ExceptionHandler(IllegalArgumentException.class)
+
+    // 400
+    @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<Object> handleBadRequest(IllegalArgumentException ex) {
-		return buildResponse(HttpStatus.BAD_REQUEST, "Requête invalide", ex.getMessage(), null);
+        return buildResponse(HttpStatus.BAD_REQUEST, "Requête invalide", ex.getMessage(), null);
     }
-	
-	// 401
-	@ExceptionHandler(UnauthorizedException.class)
+
+    // 401
+    @ExceptionHandler(UnauthorizedException.class)
     public ResponseEntity<Object> handleUnauthorized(UnauthorizedException ex) {
-		return buildResponse(HttpStatus.UNAUTHORIZED, "Il faut pour cela être identifié pour y avoir accès", ex.getMessage(), null);
+        return buildResponse(HttpStatus.UNAUTHORIZED, "Il faut pour cela être identifié pour y avoir accès", ex.getMessage(), null);
     }
-	
-	// 403
-	@ExceptionHandler(AccessDeniedException.class)
+
+    // 403
+    @ExceptionHandler(AccessDeniedException.class)
     public ResponseEntity<Object> handleForbidden(AccessDeniedException ex) {
-		return buildResponse(HttpStatus.FORBIDDEN, "Vous n'avez pas accès à la ressource", ex.getMessage(), null);
+        return buildResponse(HttpStatus.FORBIDDEN, "Vous n'avez pas accès à la ressource", ex.getMessage(), null);
     }
 
     // Catch-all
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> handleGeneric(Exception ex) {
-    	return buildResponse(HttpStatus.INTERNAL_SERVER_ERROR, "Vous n'avez pas accès à la ressource", ex.getMessage(), null);
+        return buildResponse(HttpStatus.INTERNAL_SERVER_ERROR, "Vous n'avez pas accès à la ressource", ex.getMessage(), null);
     }
-	
-	@ExceptionHandler(InvalidCredentialsException.class)
+
+    @ExceptionHandler(InvalidCredentialsException.class)
     public ResponseEntity<Object> handleInvalidCredentials(InvalidCredentialsException ex) {
-		return buildResponse(HttpStatus.UNAUTHORIZED, "Le mot de passe ne correspond pas à celui existant en base", ex.getMessage(), null);
+        return buildResponse(HttpStatus.UNAUTHORIZED, "Le mot de passe ne correspond pas à celui existant en base", ex.getMessage(), null);
     }
-	
-	@ExceptionHandler(TechnicalException.class)
-	public ResponseEntity<Object> handleTechnicalException(TechnicalException ex) {
-		return buildResponse(HttpStatus.INTERNAL_SERVER_ERROR, "Le mot de passe ne correspond pas à celui existant en base", ex.getMessage(), null);
-	}
-	
-	// Mauvais paramètre d'URL ou type d'entrée (400)
+
+    @ExceptionHandler(TechnicalException.class)
+    public ResponseEntity<Object> handleTechnicalException(TechnicalException ex) {
+        return buildResponse(HttpStatus.INTERNAL_SERVER_ERROR, "Le mot de passe ne correspond pas à celui existant en base", ex.getMessage(), null);
+    }
+
+    // Mauvais paramètre d'URL ou type d'entrée (400)
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ResponseEntity<Object> handleTypeMismatch(MethodArgumentTypeMismatchException ex) {
-    	return buildResponse(HttpStatus.BAD_REQUEST, "Type de paramètre incorrect", ex.getMessage(), null);
+        return buildResponse(HttpStatus.BAD_REQUEST, "Type de paramètre incorrect", ex.getMessage(), null);
     }
-	
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {    
-	    Map<String, Object> fieldErrors = Map.of("fieldErrors", ex.getBindingResult().getFieldErrors().stream()
-	    	.collect(Collectors.toMap(
-	    	    FieldError::getField, 
-	    	    FieldError::getDefaultMessage,
-	    	    (existing, replacement) -> existing
-	    	)));
 
-	    return buildResponse(HttpStatus.BAD_REQUEST, "Erreur de validation", "Des champs requis sont manquants ou invalides.", fieldErrors);
-	}
-	
-	private ResponseEntity<Object> buildResponse(HttpStatus status, String error, String message, @Nullable Map<String, ?> fieldErrors) {
-		Map<String, Object> bodyError = new HashMap<>();
-		bodyError.put("timestamp", LocalDateTime.now());
-		bodyError.put("status", status.value());
-		bodyError.put("message", message);
-		bodyError.put("error", error);
-		if (fieldErrors != null) bodyError.putAll(fieldErrors);
-		return new ResponseEntity<>(bodyError, status);
-	}
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {    
+        Map<String, Object> fieldErrors = Map.of("fieldErrors", ex.getBindingResult().getFieldErrors().stream()
+            .collect(Collectors.toMap(
+                FieldError::getField, 
+                FieldError::getDefaultMessage,
+                (existing, replacement) -> existing
+            )));
+
+        return buildResponse(HttpStatus.BAD_REQUEST, "Erreur de validation", "Des champs requis sont manquants ou invalides.", fieldErrors);
+    }
+
+    private ResponseEntity<Object> buildResponse(HttpStatus status, String error, String message, @Nullable Map<String, ?> fieldErrors) {
+        Map<String, Object> bodyError = new HashMap<>();
+        bodyError.put("timestamp", LocalDateTime.now());
+        bodyError.put("status", status.value());
+        bodyError.put("message", message);
+        bodyError.put("error", error);
+        if (fieldErrors != null) bodyError.putAll(fieldErrors);
+        return new ResponseEntity<>(bodyError, status);
+    }
 }
