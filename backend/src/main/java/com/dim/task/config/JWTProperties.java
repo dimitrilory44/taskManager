1e493707f032b850ca4337bdb55be430a121498d
LORY <lorydim44@gmail.com>
2025-04-22 12:33:57 +0200

    chore: reindentation fichier config

--- backend/src/main/java/com/dim/task/config/JWTProperties.java
+++ backend/src/main/java/com/dim/task/config/JWTProperties.java
@@ -1,12 +1,13 @@
 package com.dim.task.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-
 import lombok.Data;
 
 @Data
 @ConfigurationProperties(prefix = "jwt")
 public class JWTProperties {
-	private String secret;
+    
+    private String secret;
+    
     private long expiration;
 }
